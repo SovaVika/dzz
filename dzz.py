1aaf5aac3233a5039e18cec97a99ce75d0a62115
Sova Vika <sovavika1106@gmail.com>
2024-03-26 13:27:20 +0200

    commit

--- dzz.py
+++ dzz.py
@@ -3,7 +3,3 @@ class Vehicle:
         self.make = make
         self.model = model
         self.year = year
-    def print(self):
-        veh_inf = f"Vehicle:\nВиробник - {self.make}\nМодель - {self.model}"
-        print(veh_inf)
-        return veh_inf
