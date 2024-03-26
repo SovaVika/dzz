57b9339fc86b3e04edf302713834b15d1d4c7d03
Sova Vika <sovavika1106@gmail.com>
2024-03-26 13:31:56 +0200

    commit

--- dz.py
+++ dz.py
@@ -1,16 +1,16 @@
-from dzz import Vehicle
+import dzz
 
-class Car(Vehicle):
+class Car(dzz.Vehicle):
     def __init__(self, num_doors, num_wheels):
-        Vehicle.__init__(self, "Lamborghini", "Aventador", 2022) 
+        dzz.Vehicle.__init__(self, "Lamborghini", "Aventador", 2022) 
         self.num_doors = num_doors
         self.num_wheels = num_wheels
     def __str__(self):
         return f"Авто:\nКількість дверей - {self.num_doors}\nКількість коліс - {self.num_wheels}\nВиробник - {self.make}\nМодель - {self.model}\nРік - {self.year}\n"
 
-class Motorcycle(Vehicle):
+class Motorcycle(dzz.Vehicle):
     def __init__(self, engine_size, num_wheels, color):
-        Vehicle.__init__(self, "Honda", "SH 125", 2001) 
+        dzz.Vehicle.__init__(self, "Honda", "SH 125", 2001) 
         self.engine_size = engine_size
         self.num_wheels = num_wheels
         self.color = color
